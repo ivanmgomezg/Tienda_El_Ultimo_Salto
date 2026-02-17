14ebfaa8ed17a88cd06bd7f3cb78672d986a79ba
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-17 10:32:35 -0500

    Solucion ejercicio 02

---
+++ SQL/02_Ejercicio.sql
@@ -0,0 +1,10 @@
+-- Ejercicio 02: Enlista todos los productos de la categoría "Paracaídas" con sus precios, ordenados de mayor a menor precio.
+
+SELECT 
+	id_producto,
+    nombre,
+    categoria,
+    precio
+from productos
+where categoria = 'Paracaídas'
+order by precio desc;
