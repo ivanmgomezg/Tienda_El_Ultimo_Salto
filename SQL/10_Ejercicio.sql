885d70d12fb4d5173bf9d07afe60c3dc9fe6f48c
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-17 20:34:29 +0000

    Solucion ejercicio 10

---
+++ SQL/10_Ejercicio.sql
@@ -0,0 +1,5 @@
+--Ejercicio 10: Muestra todos los clientes de género "F" (femenino) ordenados por fecha de nacimiento (más viejitas primero).
+
+SELECT * from clientes
+where genero='F'
+order by fecha_nacimiento asc
