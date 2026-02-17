bfffbb9d280b54c62bf84ebc252b5cbbe3aa07fa
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-17 21:02:27 +0000

    Solucion ejercicio 13

---
+++ 13_Ejercicio.sql
@@ -0,0 +1,5 @@
+--Ejercicio 13: ¿Cuántos productos tenemos en la categoría "Paracaídas"?
+
+SELECT COUNT(*) as 'Productos de la categoria paracaídas'
+FROM productos
+WHERE categoria = 'Paracaídas';
