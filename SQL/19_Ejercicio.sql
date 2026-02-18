310235bb5f103ce5fd5c56eda3d5dd2081e9e032
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-18 17:46:29 +0000

    Solucion ejercicio 19

---
+++ SQL/19_Ejercicio.sql
@@ -0,0 +1,10 @@
+--Ejercicio 19: ¿Cuál es el producto más vendido en cantidad (en términos de unidades, no dinero)?
+
+SELECT 
+	productos.id_producto,
+    productos.nombre,
+    SUM(detalle_ventas.cantidad) as 'Total_Ventas'
+from detalle_ventas
+JOIN productos on detalle_ventas.id_producto = productos.id_producto
+GROUP BY productos.id_producto
+order by Total_Ventas DESC
