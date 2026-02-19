e79ba96fbab9b14050cc476f9c6dda3e28fe0148
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-19 15:50:10 +0000

    Solucion ejercicio 20

---
+++ SQL/20_Ejercicio.sql
@@ -0,0 +1,12 @@
+--Ejercicio 20: Para cada categoría de producto, calcula el ingreso total, cantidad de unidades vendidas 
+-- y ticket promedio por venta.
+
+select 
+  productos.categoria,
+  SUM(detalle_ventas.cantidad) AS "Cantidades",
+  SUM(detalle_ventas.precio_unitario * detalle_ventas.cantidad) AS "Ingreso_Total",
+  AVG(detalle_ventas.precio_unitario * detalle_ventas.cantidad) as "Promedio_de_Venta"
+from detalle_ventas
+JOIN productos ON detalle_ventas.id_producto = productos.id_producto
+group by productos.categoria
+order by Ingreso_Total
