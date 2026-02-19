4351803665949e353301993590fa1ecb06a5609b
ivanmgomezg <ivanm.gomezg@gmail.com>
2026-02-19 16:36:46 +0000

    Solucion ejercicio 21

---
+++ SQL/21_Ejercicio.sql
@@ -0,0 +1,11 @@
+--Ejercicio 21: ¿Cuáles son los clientes que han usado el cupón "ULTIMO_SUSPIRO" al menos 3 veces?
+
+select 
+	clientes.nombre, 
+	COUNT(*) as "Cant_Cupones"
+from ventas
+join clientes on ventas.id_cliente = clientes.id_cliente
+WHERE ventas.cupon_usado = 'ULTIMO_SUSPIRO'
+group by clientes.nombre
+HAVING COUNT(*) >= 3 --HAVING COUNT(ventas.cupon_usado) >= 3 
+ORDER by Cant_Cupones DESC
